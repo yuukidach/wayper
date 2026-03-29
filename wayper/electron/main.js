7308092166c2ddbf776396612f4e786bbbfa7311
Da Chen <dachen@connect.hku.hk>
2026-03-29 20:48:34 +0800

    fix: auto-hide menu bar on Linux

--- wayper/electron/main.js
+++ wayper/electron/main.js
@@ -97,6 +97,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    autoHideMenuBar: !isMac,
     titleBarStyle: isMac ? 'hiddenInset' : 'default',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
